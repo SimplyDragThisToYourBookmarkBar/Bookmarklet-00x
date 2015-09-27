07c2a78418f62ed380b19a0ecb98e166eb264951
SimplyDragThisToYourBookmarkBar! <simplydragthistoyourbookmarks@gmail.com>
2015-09-27 20:34:00 +0200

    Update 001.js

--- 001.js
+++ 001.js
@@ -1,17 +1,10 @@
-javascript:void((function(w){
+javascript:void((function(global){
 
- var d=w.document,
- e, i=0, a=[], b=a.slice.call(d.getElementsByTagName('*')), l=b.length+0,
- n, p=/(HTML|HEAD|META|TITLE|STYLE|SCRIPT|BASE|NOSCRIPT|NOFRAME|PARAM)/;
+ var slice=Array.prototype.slice,
+ $w=global, computedStyleOf=$w.getComputedStyle,
+ $d=$w.document, $html=$d.documentElement, $body=$d.body,
+ tags=[$html,$body].concat(slice.call($body.getElementsByTagName('*')));
 
- while(i<l){
-	e=b[i++];
-	n=e.tagName;
-	if('style' in e && p.test(n)===false){
-	 a[a.length]=e;
-	};
- };
-
- console.dir(a);
+ console.dir(tags);
 
 })(window));
