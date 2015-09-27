bb1857010db26ebda6811ae95a48ed0212d40401
SimplyDragThisToYourBookmarkBar! <simplydragthistoyourbookmarks@gmail.com>
2015-09-27 22:52:40 +0200

    Update 001.js

--- 001.js
+++ 001.js
@@ -1,15 +1,31 @@
-	if(eTagName in defaultStyles){
-			ds=defaultStyles[eTagName];
-		}else{
-			sandBox=sandBox||(sandBox=$body.appendChild($d.createElement('iframe')));
-			console.log(sandBox);
-		};
+javascript:void((function(global){
 
+ var $w=global, $d=$w.document, $html=$d.documentElement, $body=$d.body,
+ slice=$w.Array.prototype.slice,
+ tags=[$html,$body].concat(slice.call($body.getElementsByTagName('*'))),
+ tag2skip=/STYLE|SCRIPT|SOURCE|PARAM|NO(SCRIPT|FRAME|EMBED)|RP|AREA|TRACK|DATALIST|TEMPLATE/,
+ l=tags.length, i=0, e, eTagName, ds,
+ sandBox=$body.appendChild($d.createElement('iframe')),
+ sandBoxedDoc=sandBox.contentDocument,
+ sandBoxedBody=sandBoxedDoc.body,
+ sandBoxedWindow=sandBox.contentWindow,
+ defaultStylesReference=$w.defaultStylesReference||($w.defaultStylesReference={});
+
+
+ while(i<l){
+	e=tags[i++];
+	eTagName=e.tagName;
+	if(tag2skip.test(eTagName)===false){
+		if(eTagName in defaultStylesReference){
+			ds=defaultStylesReference[eTagName];
+		}else{
+
+			ds=defaultStylesReference[eTagName]=sandBoxedWindow.getComputedStyle(sandBoxedBody.appendChild(sandBoxedDoc.createElement(eTagName)));
+
+			console.log(ds);
+		};
 	};
-	if(sandBox){$body.removeChild(sandBox);}
  };
 
- console.log('usedTags',Object.keys(defaultStyles));
- console.dir(defaultStyles);
-
+ $body.removeChild(sandBox);
 })(window));
