d14c14c9cf0dcc73633a5b0b537a6b53106dac81
SimplyDragThisToYourBookmarkBar! <simplydragthistoyourbookmarks@gmail.com>
2015-09-27 23:13:59 +0200

    Update 001.js

--- 001.js
+++ 001.js
@@ -2,6 +2,7 @@ javascript:void((function(global){
 
  var $w=global, $d=$w.document, $html=$d.documentElement, $body=$d.body,
  slice=$w.Array.prototype.slice,
+ computedStyleOf=$w.getComputedStyle,
  tags=[$html,$body].concat(slice.call($body.getElementsByTagName('*'))),
  tag2skip=/STYLE|SCRIPT|SOURCE|PARAM|NO(SCRIPT|FRAME|EMBED)|RP|AREA|TRACK|DATALIST|TEMPLATE/,
  l=tags.length, i=0, e, eTagName, ds,
@@ -9,7 +10,8 @@ javascript:void((function(global){
  sandBoxedDoc=sandBox.contentDocument,
  sandBoxedBody=sandBoxedDoc.body,
  sandBoxedWindow=sandBox.contentWindow,
- defaultStylesReference=$w.defaultStylesReference||($w.defaultStylesReference={});
+ defaultStylesReference=$w.defaultStylesReference||($w.defaultStylesReference={}),
+ cs,j,jL,pn;
 
 
  while(i<l){
@@ -19,13 +21,42 @@ javascript:void((function(global){
 		if(eTagName in defaultStylesReference){
 			ds=defaultStylesReference[eTagName];
 		}else{
-
 			ds=defaultStylesReference[eTagName]=sandBoxedWindow.getComputedStyle(sandBoxedBody.appendChild(sandBoxedDoc.createElement(eTagName)));
+		};
+		cs=computedStyleOf(e);
+		j=0;
+		jL=cs.length+0;
+		while(j<jL){
+			pn=cs[j++];
+			if(cs[pn] !== ds[pn]){
 
-			console.log(ds);
+			};
 		};
 	};
  };
 
+
  $body.removeChild(sandBox);
 })(window));
+//===================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+javascript:void((function(global){})(window));
+
+
+ console.log('usedTags',Object.keys(defaultStylesReference));
+ console.dir(defaultStylesReference);
+
+
+
+
